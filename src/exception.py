3e430876b8d6f0125a3a43d27d156cb4d3423add
Sujeet <sontakkesujeet@gmail.com>
2023-04-05 11:51:14 +0530

    logger and Exception

---
+++ src/exception.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def error_messsage_details(error,error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in python script name [{0}] line number [{}] error message [{}]".format(
+        file_name,exc_tb.tb_lineno,str(error))
+
+    return error_message
+
+
+
+class CustomeException(Exception):
+
+    def __init__(self,error_message,error_detail:sys) -> None:
+        super().__init__(error_message)
+        self.error_message = error_messsage_details(error_message,error_detail=error_detail)
+
+
+    def __str__(self) -> str:
+        return self.error_message
